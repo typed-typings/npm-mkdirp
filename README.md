c22e0a7cf2597d5fc9122ca70af19b6638886fc0
Blake Embrey <hello@blakeembrey.com>
2015-12-16 14:19:04 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typed Mkdirp
+
+The type definition for [mkdirp](https://github.com/substack/node-mkdirp).
+
+## License
+
+MIT
---
+++ index.d.ts
@@ -0,0 +1,13 @@
+declare function mkdirp (path: string, cb: (err: Error, made: string) => any): void;
+declare function mkdirp (path: string, opts: mkdirp.Opts, cb: (err: Error, made: string) => any): void;
+
+declare module mkdirp {
+  export interface Opts {
+    fs: any;
+    mode: number;
+  }
+
+  export function sync (path: string, opts: mkdirp.Opts): string;
+}
+
+export = mkdirp;
